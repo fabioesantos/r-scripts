a738955b4de23d2eedaef5ed23f7aa4d8a3c7ff7
Fabio Eduardo dos Santos <41553663+fabioesantos@users.noreply.github.com>
2020-05-07 17:28:59 -0300

    Add files via upload
    R script de exemplo de análise de variância.

---
+++ Análise de Variância.R
@@ -0,0 +1,43 @@
+# Pacotes usados aqui
+install.packages('car')
+install.packages('sciplot')
+
+data("PlantGrowth")
+dados <- PlantGrowth
+
+
+# Analise de Variância(ANOVA)
+
+# ANOVA para a variável resposta "response"
+anova <- aov(weight ~ group, data = dados)
+# Ver o resumo da ANOVA
+summary(anova)
+
+### Análise dos resíduos da Anova/Testes dos Presupostos da ANOVA ###
+
+# shapiro-wilk normality test
+shapiro.test(resid(anova))
+
+# Histograma dos resíduos da ANOVA
+hist(resid(anova))
+
+# qqplot dos resíduos da ANOVA
+qqnorm(resid(anova))
+qqline(resid(anova))
+
+# Bartlett test of homogeneity of variances
+bartlett.test(residuals(anova) ~ dados$group)
+# análise gráfica dos resíduos
+par(mfrow=c(2,2))
+plot(anova)
+
+# Levene Test of homogeneity of variances (Outro teste de homocedasticidade)
+library(car)
+leveneTest(anova)
+
+# Fazer o gráfico de barras já com a barra de erro do desvio padrão
+library(sciplot)
+bargraph.CI(x.factor = factor(group), response = weight, data = data,
+            main = "Plant Growth", xlab = "Group",
+            ylab = "Weight", col = "grey",  legend = TRUE)
+
